b022f3722e82e661461fe84f340baa2553009d19
Natasha Robarge <robarge.natasha7@gmail.com>
2017-10-17 10:46:23 -0500

    Added get dogs function in dogs route

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,8 +1,21 @@
 const db = require('../models/dog');
 
 const DogtRoutes = require('./routes/dogs');
-    app.get('/dogs', );
-    app.post('/dogs', );
-    app.get('/dogs/:id', );
-    app.put('/dogs/:id', );
-    app.delete('/dogs/:id', );
+
+function getDogs(req, res) {
+  petfinder.findPet(78721, {}, function(err, animals) {
+    var result = [];
+    animals.forEach(function(animal) {
+      for(var prop in animal) {
+        if(animal[prop] === 'Dog') {
+          result.push({ 'animal': animal })
+        }
+      }
+    })
+    res.json(result);
+  });
+}
+
+module.exports = {
+  getDogs: getDogs
+}
