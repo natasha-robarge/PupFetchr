462a6f5fe412015d25a11f1f01d1f4d6f4db8ba5
Andrew Chavez <chavandrew@gmail.com>
2017-10-17 10:41:26 -0500

    fix spacing and semi-colons

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 var petfinder = require('petfinder')('a839717f845b6ee822240f22a2b5a84c', '6debdbd0c30c89132579a1a722d970ac');
 require('dotenv').config();
+
 //sets up mongoose connection
 mongoose.connection.openUri(process.env.DB_CONN, function(err, conn) {
   if (err) {
@@ -13,19 +14,23 @@ mongoose.connection.openUri(process.env.DB_CONN, function(err, conn) {
     console.log('Successfully connected');
   }
 });
+
 //sets up express and port
 const app = express();
 const port = process.env.PORT || 3000;
+
 //Uses public folder
-app.use('/', express.static('public'))
+app.use('/', express.static('public'));
+
 //App routes
-const userRoutes = require('../routes/users');
+const userRoutes = require('./routes/users');
 app.get('/', userRoutes.getUser);
 app.get('/', userRoutes.getUsers);
 app.post('/', userRoutes.createUser);
 app.put('/', userRoutes.updateUser);
 app.delete('/', userRoutes.removeUser);
 app.get('/', userRoutes.newLoginSession);
+
 app.get('/', function(req, res) {
   petfinder.findPet(78721, {}, function(err, animals) {
     var result = [];
@@ -39,7 +44,8 @@ app.get('/', function(req, res) {
     res.json(result);
   });
 });
-//App Start
+
+
 //App Start
 app.listen(3000, function() {
   console.log('On port 3000');
